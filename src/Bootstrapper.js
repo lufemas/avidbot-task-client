052c36a9ce9626bc913d7a503f4d1d76fd208111
Luis Fernando Maschietto Junior <maschietto.jr@gmail.com>
2023-02-27 15:59:44 +0000

    Update Bootstrapper.js and Header.js to use on replit public version

--- src/Bootstrapper.js
+++ src/Bootstrapper.js
@@ -4,56 +4,58 @@ import Header from "./Header";
 
 import { io } from "socket.io-client";
 import { useEffect, useState } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function Bootstrapper() {
   const [socket, setSocket] = useState(null);
-  const [subtitle, setSubtitle] = useState('')
-  const [isRunning, setIsRunning] = useState(false)
-
+  const [subtitle, setSubtitle] = useState("");
+  const [isRunning, setIsRunning] = useState(false);
 
   useEffect(() => {
-    const newSocket = io(`wss://avidbot-task-server.herokuapp.com`);
+    const newSocket = io(`wss://avidbot-task-server--lufemas.repl.co/`);
     setSocket(newSocket);
     return () => newSocket.close();
   }, [setSocket]);
   return (
     <>
-    <main>
-      <Header subtitle={subtitle} isRunning={isRunning} />
-       { socket ? (
-        <>
-    <Router>
+      <main>
+        <Header subtitle={subtitle} isRunning={isRunning} />
+        {socket ? (
+          <>
+            <Router>
+              {/* Two paths, the Map Input and the App Sumilator */}
+              <Switch>
+                <Route exact path="/">
+                  <MapInput socket={socket} setSubtitle={setSubtitle} />
+                </Route>
+                <Route path="/app">
+                  <App
+                    socket={socket}
+                    setSubtitle={setSubtitle}
+                    setIsRunning={setIsRunning}
+                  />
+                </Route>
+              </Switch>
+            </Router>
+          </>
+        ) : (
+          <div>No server found...</div>
+        )}
+      </main>
 
-          {/* Two paths, the Map Input and the App Sumilator */}
-          <Switch>
-          <Route exact path="/" >
-            <MapInput socket={socket} setSubtitle={setSubtitle} />
-          </Route>
-          <Route path="/app">
-           <App socket={socket} setSubtitle={setSubtitle} setIsRunning={setIsRunning}/>
-
-          </Route>
-        </Switch>
-
-
-    </Router>
-        </>
-      ) : (
-        <div>No server found...</div>
-      )}
-    </main>
-
-    <footer style={{textAlign:'center', fontSize:'.75em', marginTop:'8em'}}>
-      <h3><strong>Avidbot Programming Challenge</strong></h3>
-      <p style={{margin:'0'}}><strong>Candidate:</strong> Luis Fernando Maschietto Junior</p> 
-      <p style={{margin:'0'}}><strong>email:</strong> maschietto.jr@gmail.com</p> 
-    </footer>
+      <footer
+        style={{ textAlign: "center", fontSize: ".75em", marginTop: "8em" }}
+      >
+        <h3>
+          <strong>Avidbot Programming Challenge</strong>
+        </h3>
+        <p style={{ margin: "0" }}>
+          <strong>Candidate:</strong> Luis Fernando Maschietto Junior
+        </p>
+        <p style={{ margin: "0" }}>
+          <strong>email:</strong> maschietto.jr@gmail.com
+        </p>
+      </footer>
     </>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,36 +1,51 @@
-import {
-  SyncOutlined,
-  LoadingOutlined,
-} from '@ant-design/icons';
+import { SyncOutlined, LoadingOutlined } from "@ant-design/icons";
 
-function Header({subtitle, isRunning}) {
-
+function Header({ subtitle, isRunning }) {
   const headerStyle = {
-    display: 'flex',
-    justifyContent: 'left',
-    alignItems: 'center',
-    padding: '.5em 1em',
-    marginBottom: '.5em',
-    backgroundColor: 'var(--green)',
-    color: 'black'
-  }
+    display: "flex",
+    justifyContent: "left",
+    alignItems: "center",
+    padding: ".5em 1em",
+    marginBottom: ".5em",
+    backgroundColor: "var(--green)",
+    color: "black",
+  };
 
   const paddingZero = {
-    padding: '0'
-  }
+    padding: "0",
+  };
 
   const mLeft1 = {
-    margin: '0 0 0 1em'
-  }
+    margin: "0 0 0 1em",
+  };
 
   return (
     <header style={headerStyle}>
-      <h1 style={{fontWeight: 'Bold', textAlign:'left', fontSize:'1.5em', ...mLeft1, ...paddingZero}}>Simbot</h1>
-      <h2 style={{...mLeft1, ...paddingZero, fontSize:'1.1em'}}>{subtitle}</h2>
-      {isRunning ?
-      <LoadingOutlined spin style={{color: 'white', fontSize:'1.5em', ...mLeft1, ...paddingZero}} />
-      : null
-    }
+      <h1
+        style={{
+          fontWeight: "Bold",
+          textAlign: "left",
+          fontSize: "1.5em",
+          ...mLeft1,
+          ...paddingZero,
+        }}
+      >
+        Simbot (replit public version)
+      </h1>
+      <h2 style={{ ...mLeft1, ...paddingZero, fontSize: "1.1em" }}>
+        {subtitle}
+      </h2>
+      {isRunning ? (
+        <LoadingOutlined
+          spin
+          style={{
+            color: "white",
+            fontSize: "1.5em",
+            ...mLeft1,
+            ...paddingZero,
+          }}
+        />
+      ) : null}
     </header>
   );
 }
